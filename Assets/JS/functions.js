dfc74d047b96acc1e77af77b4ac32bc1600c6767
Bawandeep Singh <Singhbawan48@gmail.com>
2022-08-09 02:59:54 -0400

    completed css for drop down mwnu ; created update weather card function

--- Assets/JS/functions.js
+++ Assets/JS/functions.js
@@ -4,8 +4,13 @@ var units = "imperial";
 function searchEventHandler(e) {
   e.preventDefault();
   let searchInput = document.querySelector("#searchInput");
+  clearSearchResult();
   getLatLong(searchInput.value);
   searchInput.value = "";
+}
+
+function clearSearchResult() {
+  document.getElementById("searchResults").innerText = "";
 }
 
 function getcurrentlocation() {
@@ -68,8 +73,6 @@ function getLatLong(name) {
           lon: element.lon,
         };
       });
-
-      console.log("value sent from getLatLong Function", coordArr);
       getWeatherData(coordArr);
     });
 }
@@ -84,4 +87,68 @@ async function weatherApiCall(coordObj) {
   let response = await fetch(url);
   let json1 = await response.json();
   return json1;
+}
+
+// Input : weather data json recieved from API call in rew
+// Return : Processed object with relevant data.
+
+function parseWeatherData(json1) {
+  let result = {
+    cityName: json1.name,
+    countryName: json1.sys.country,
+    weatherIcon: json1.weather[0].icon,
+    weatherDescription: json1.weather[0].description,
+    tempUnits: units === "imperial" ? "&#8457;" : "&#8451;",
+    temp: Math.floor(json1.main.temp),
+    lon: json1.coord.lon,
+    lat: json1.coord.lat,
+  };
+  return result;
+}
+
+function renderDropDownMenu(element) {
+  var resultsEl = document.getElementById("searchResults");
+  resultsEl.setAttribute("class", "visible");
+  var resultEl = document.createElement("li");
+  let data = parseWeatherData(element);
+  resultEl.setAttribute("data-lat", `${data.lat}`);
+  resultEl.setAttribute("data-lon", `${data.lon}`);
+  resultEl.setAttribute("class", "dropdown result");
+  let countryNameLC = data.countryName.toLowerCase();
+  resultEl.innerHTML = `
+    <span>${data.cityName}, ${data.countryName} <img width="20px" height="15px" src="https://openweathermap.org/images/flags/${countryNameLC}.png"></span>
+    <span>${data.temp}${data.tempUnits}</span>
+    <span> <img width ="50px" height = "50px" src="https://openweathermap.org/img/wn/${data.weatherIcon}@2x.png"> </span>
+    <span>${data.lat}, ${data.lon}</span>
+  `;
+  resultsEl.appendChild(resultEl);
+  resultsEl.addEventListener("click", updateWeatherCard);
+}
+
+async function updateWeatherCard(e) {
+  var optionSelected = e.target;
+  if (optionSelected.matches(".result")) {
+    var coordObj = {
+      lat: optionSelected.dataset.lat,
+      lon: optionSelected.dataset.lon,
+    };
+    let result = await weatherApiCall(coordObj);
+    let jsonresult = await result;
+    let data = parseWeatherData(jsonresult);
+    renderWeatherCard(data);
+    clearSearchResult();
+  }
+}
+
+function getWeatherData(coordArr) {
+  let cities = [];
+  coordArr.forEach((element, i) => {
+    cities[i] = weatherApiCall(element);
+  });
+
+  Promise.all(cities).then((results) => {
+    results.forEach((element) => {
+      renderDropDownMenu(element);
+    });
+  });
 }
--- Assets/JS/test.js
+++ Assets/JS/test.js
@@ -12,52 +12,12 @@ function renderSearchError() {
 }
 
 //======================Sections=============//
-function getWeatherData(coordArr) {
-  let cities = [];
-  coordArr.forEach((element, i) => {
-    cities[i] = weatherApiCall(element);
-  });
-
-  Promise.all(cities).then((results) => {
-    results.forEach((element) => {
-      renderDropDownMenu(element);
-    });
-  });
-
-  console.log("Function not ready");
-}
 
 //======================Sections=============//
-
-function renderDropDownMenu(element) {
-  var resultsEl = document.getElementById("searchResults");
-  var resultEl = document.createElement("li");
-  let data = parseWeatherData(element);
-  let countryNameLC = data.countryName.toLowerCase();
-  resultEl.innerHTML = `
-  <span>${data.cityName}, ${data.countryName} <img src="https://openweathermap.org/images/flags/${countryNameLC}.png"></span>
-  <span>${data.temp}${data.tempUnits}</span>
-  <span> <img src="https://openweathermap.org/img/wn/${data.weatherIcon}@2x.png"> </span>
-  <span>${data.lat}, ${data.lon}</span>
-`;
-  resultsEl.appendChild(resultEl);
-  console.log(" render Drop Down Menu Function not ready");
+function renderWeatherCard(weatherData) {
+  console.log(weatherData);
 }
-
 //======================Sections=============//
-function parseWeatherData(json1) {
-  let result = {
-    cityName: json1.name,
-    countryName: json1.sys.country,
-    weatherIcon: json1.weather[0].icon,
-    weatherDescription: json1.weather[0].description,
-    tempUnits: units === "imperial" ? "&#8457;" : "&#8451;",
-    temp: Math.floor(json1.main.temp),
-    lon: json1.coord.lon,
-    lat: json1.coord.lat,
-  };
-  return result;
-}
 
 //======================Sections=============//
 
